c534062d66ff105dc118b854fb609de6627fff8d
Akhilesh Kumar Singh <100690788+Akhileshkumarsingh21221@users.noreply.github.com>
2023-03-21 21:11:25 +0530

    Add files via upload

---
+++ csesproblems.cpp
@@ -0,0 +1,101 @@
+// #include<bits/stdc++.h>
+// #define ll long long
+// using namespace std;
+// int first_occurrance(vector<int>v,int target){
+//     int start=0;
+//     int end=v.size()-1;
+//     int result=-1;
+//     while(start<=end){
+//         int mid=start+(end-start)>>1;
+//         if(v[mid]==target){
+//         result=mid;
+//         end=mid-1;}
+//         else if(v[mid]>target)
+//         end=mid-1;
+//         else if(v[mid]<target){
+//             start=mid+1;
+//         }
+        
+
+//     }
+// return result;
+// }
+// int main(){
+// vector<int>v={1,3,5,10,10,10,10,10,10,10,13,15};
+// int target=10;
+// cout<<first_occurrance(v,target)<<endl;
+// return 0;
+// }
+// #include<bits/stdc++.h>
+// #define ll long long
+// using namespace std;
+// ll binpower(ll a,ll b){
+// //     if(b==0)
+// //     return 1;
+// //     ll res=binpower(a,b/2);
+// //     if(b%2)
+// //     return res*res*a;
+// //     else 
+// //     return res*res;}
+// //     int main(){
+// // cout<<binpower(3,10);
+    
+
+// // return 0;
+// // }
+
+// // #include<bits/stdc++.h>
+// // #define ll long long
+// // using namespace std;
+// // ll power(int a,int b){
+// // ll res=1;
+// // while(b>0){
+// //     if(b&1)
+// //         res=res*a;
+// //         a=a*a;
+// //         b>>=1;
+    
+// // }
+// //     return res;
+
+// // }
+// // int main(){
+// // cout<<power(2,10);
+// // return 0;
+// // }
+
+// //cses problem 1  
+// //weird algorithm
+
+
+// #include<bits/stdc++.h>
+// #define ll long long
+// using namespace std;
+// int main(){
+// int n;
+// cin>>n;
+// while(n!=1){
+// if(n%2){
+//     cout<<n<<" ";
+//     n=n*3+1;}
+//     else{
+//         cout<<n<<" ";
+//         n=n/2;
+// }
+
+// }
+// cout<<"1"<<endl;
+// return 0;
+// }
+
+
+//missing number
+
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+int main(){
+//o(nlogn)
+
+return 0;
+}
